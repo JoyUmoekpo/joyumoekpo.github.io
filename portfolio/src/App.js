493635d3b0c558965b228100491745798e51ce10
Joy Umoekpo <joyumoekpo1@gmail.com>
2022-07-26 09:59:33 -0500

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg'
-import './App.scss'
-import {
-  Routes
-} from 'react-router-dom'
+import logo from './logo.svg';
+import './App.scss';
+import { Routes } from 'react-router-dom';
 
 function App() {
   return (
