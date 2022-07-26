eec166a8950a873130b4f926fb54dd63611d3b74
Joy Umoekpo <joyumoekpo1@gmail.com>
2022-07-26 18:43:02 -0500

    Changed homepage content

--- portfolio/src/componenets/Home/index.js
+++ portfolio/src/componenets/Home/index.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className = "container home-page">
             <div className = "text-zone">
                 <h1> Hi, I 'm Joy Umoekpo!</h1> 
-                <h2> Front End Developer</h2> 
+                <h2> Front End Developer / Back End Developer / Full Stack Developer</h2> 
                 <Link to = "/contact" className ="flat-button">CONTACT ME</Link>
             </div>
         </div>
