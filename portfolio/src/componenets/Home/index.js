ccc62816744d35b28715424a5f1fb65a0aef4f43
Joy Umoekpo <joyumoekpo1@gmail.com>
2022-07-26 18:26:21 -0500

    Created and modified homepage

--- portfolio/src/componenets/Home/index.js
+++ portfolio/src/componenets/Home/index.js
@@ -1,1 +1,18 @@
-const Home 
+import { Link } from 'react-router-dom';
+import './index.scss'
+
+const Home = () => {
+
+
+    return ( 
+        <div className = "container home-page">
+            <div className = "text-zone">
+                <h1> Hi, I 'm Joy Umoekpo!</h1> 
+                <h2> Front End Developer</h2> 
+                <Link to = "/contact" className ="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
